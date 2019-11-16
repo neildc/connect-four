fa88cdbe402b9a21f7784f2c54461a0999303dc2
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2019-11-16 12:50:31 +1100

    Rough initial model

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,71 @@
-module Main exposing (..)
+module Main exposing (Board, Color(..), Model, Msg(..), Player, Screen(..), init, main, update, view)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
+
 
 
 ---- MODEL ----
 
 
+type Color
+    = Blue
+    | Red
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , color : Color
+    , moveTimerSeconds : Int
+    }
+
+
+type alias Board =
+    { grid : Array (Array (Maybe { playerId : Int }))
+    }
+
+
+type Screen
+    = StartScreen
+    | GameScreen { board : Board, activePlayerTurn : Player }
+    | RoundEndScreen
+        { winner : Maybe Player -- might be a tie
+        , scorePlayers : List Int -- should we handle more than a 2 players?
+        }
+
+
 type alias Model =
-    {}
+    { board : Board
+    , players : List Player
+    , screen : Screen
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    let
+        players =
+            [ { id = 0
+              , name = "Player 1"
+              , color = Blue
+              , moveTimerSeconds = 0
+              }
+            , { id = 1
+              , name = "Player 2"
+              , color = Red
+              , moveTimerSeconds = 0
+              }
+            ]
+    in
+    ( { players = players
+      , board = { grid = Array.empty }
+      , screen = StartScreen
+      }
+    , Cmd.none
+    )
 
 
 
